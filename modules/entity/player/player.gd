7966c7b7543e3067e8945254ba187ace6e9609c5
Robin <robin@norrland.xyz>
2023-04-29 23:41:21 +0200

    Added inventory system

--- modules/entity/player/player.gd
+++ modules/entity/player/player.gd
@@ -1,8 +1,13 @@
 extends CharacterBody3D
 
+#inventory
+signal toggle_inventory()
+@export var inventory_data: InventoryData
+@export var equip_inventory_data: InventoryDataEquip
 
 const SPEED = 5.0
 const JUMP_VELOCITY = 2
+var health: int = 5
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
@@ -10,15 +15,23 @@ var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 #Get imidiate child neck, camera etc
 @onready var neck := $Neck
 @onready var camera := $Neck/Camera3D
+@onready var interact_ray = $Neck/Camera3D/InteractRay
+
+func _ready():
+	PlayerManager.player = self
+	#starts game in captured mode
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 
 func _unhandled_input(event: InputEvent) -> void:
+	""" TEMPORARILY DISABLED WHILE CREATING INVENTORY
 	#Makes mouse invisible & capure
 	if event is InputEventMouseButton:
 		Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 	#Makes mouse visible when esc is pressed
 	elif event.is_action_pressed("ui_cancel"):
 		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
-	#Mouse head movement
+	"""
+	#Mouse head mov	ement
 	if Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED:
 		if event is InputEventMouseMotion:
 			#Subtract how far the mouse moved in the axisis
@@ -27,6 +40,13 @@ func _unhandled_input(event: InputEvent) -> void:
 			camera.rotate_x(-event.relative.y * 0.0025)
 			#limits neck rotation
 			camera.rotation.x = clamp(camera.rotation.x, deg_to_rad(-90), deg_to_rad(90))
+	
+	#inventory
+	if Input.is_action_just_pressed("inventory"):
+		toggle_inventory.emit()
+	
+	if Input.is_action_just_pressed("interact"):
+		interact()
 
 func _physics_process(delta):
 	# Add the gravity. 
@@ -68,3 +88,15 @@ func cameraRotation():
 	#"Inetoeproelate" between rotation target and acutal rotation to not give players whiplash
 	neck.rotation.z = lerp_angle(neck.rotation.z, target_rotation.z, ROTATION_SPEED)
 	neck.rotation.z = clamp(neck.rotation.z, deg_to_rad(-0.25), deg_to_rad(0.25))
+
+func interact() -> void:
+	if interact_ray.is_colliding():
+		interact_ray.get_collider().player_interact()
+
+
+func get_drop_position() -> Vector3:
+	var direction = -camera.global_transform.basis.z
+	return camera.global_position + direction
+
+func heal(heal_value: int) -> void:
+	health += heal_value
---
+++ modules/entity/player/player_manager.gd
@@ -0,0 +1,9 @@
+extends Node
+
+var player
+
+func use_slot_data(slot_data: SlotData) -> void:
+	slot_data.item_data.use(player)
+
+func get_global_positin() -> Vector3:
+	return player.get_global_positiond()
