18be86a4302d60f4b456e15f55a28a659a1e937b
Robin <robin@norrland.xyz>
2023-04-29 23:43:32 +0200

    fixed minor spelling mistake

--- modules/entity/player/player_manager.gd
+++ modules/entity/player/player_manager.gd
@@ -6,4 +6,4 @@ func use_slot_data(slot_data: SlotData) -> void:
 	slot_data.item_data.use(player)
 
 func get_global_positin() -> Vector3:
-	return player.get_global_positiond()
+	return player.get_global_position()
