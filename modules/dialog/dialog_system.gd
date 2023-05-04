e35a0f674c876387b94794848da10d370ac78bcd
joakim <86793266+joakimgaming@users.noreply.github.com>
2023-05-04 20:10:13 +0200

    dialogues

--- modules/dialog/dialog_system.gd
+++ modules/dialog/dialog_system.gd
@@ -4,19 +4,39 @@ extends CanvasLayer
 @onready var text = $ColorRect/text
 @onready var voice = $voice
 
-var dialogues = {}
+var dialog
+var phrase_num = 0
 
 func _ready():
-	dialogues = Dialogues.Oskar
+	dialog = get_dialog()
+	assert(dialog, "cp godot, cp godot, cp godot, cp godot")
 	play_dialog()
-
+	
 func _process(delta):
-	pass
-		
+	if Input.is_action_just_pressed("fire"):
+		play_dialog()
+
+func get_dialog(): #skicka in variabel här sen för att hämta rätt json fil
+	var dialogPath = "res://modules/dialog/dialogues/test.json"
+	var file = FileAccess.open(dialogPath, FileAccess.READ)
+	var json = file.get_as_text()
+	var output = JSON.parse_string(json)
+	
+	if typeof(output) == TYPE_ARRAY:
+		return output
+	else:
+		print("cp godot")
+
 func play_dialog():
-	character_name.text = dialogues["name"]
-	text.text = dialogues["text"]
-	voice.stream = load(dialogues["voice"])
+	if phrase_num >= len(dialog):
+		queue_free()
+		return
+	
+	character_name.text = dialog[phrase_num][0]["name"]
+	text.text = dialog[phrase_num][0]["text"]
+	voice.stream = load(dialog[phrase_num][0]["voice"])
 	$voice.play()
+	phrase_num += 1
+	return
 
 
--- modules/dialog/dialogues.gd
+++ modules/dialog/dialogues.gd
@@ -3,5 +3,5 @@ extends Node
 var Oskar = {
 	"name":"Oskar",
 	"text":"Hej och välkomna till Sommarflörten allesammans!",
-	"voice":"res://modules/dialog/voice/Oskar/1.mp3"
+	"voice":"res://modules/dialog/voice/Oskar/1.mp3",
 	}
